315a8fda5cb377083fc9b4f6a7a5617969e637b0
iviooThong <kitipot2538@gmail.com>
2018-04-04 15:11:17 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBYXy67zuFZfvBN2jT6lOrfFbdg_Y2Dph8",
+  authDomain: "map-api-195010.firebaseapp.com",
+  databaseURL: "https://map-api-195010.firebaseio.com",
+  projectId: "map-api-195010",
+  storageBucket: "map-api-195010.appspot.com",
+  messagingSenderId: "392474629206"
+};
+
 @NgModule({
   declarations: [
     MyApp,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
+
+declare var google;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,51 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  @ViewChild("map") mapElement: ElementRef;
+  map: any;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  ionViewDidLoad() {
+    this.loadMap();
+  }
+
+  loadMap() {
+    let latLng = new google.maps.LatLng(16.245616, 103.250208);
+
+    let mapOptions = {
+      center: latLng,
+      zoom: 16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    this.addMarker();
+  }
+  
+  addMarker() {
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      // position: this.map.getCenter()
+      position: new google.maps.LatLng(16.246587, 103.247490)
+    });
+
+    let content = "<h4>Information!</h4>";
+
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content) {
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, "click", () => {
+      infoWindow.open(this.map, marker);
+    });
   }
 
 }
